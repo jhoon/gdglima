08b21dd858b8c459787fb92e491b07d9f75980bd
mdavid20@gmail.com <mdavid20@gmail.com@afbecfb3-47b9-6d02-69f1-8a0368529439>
2012-10-11 15:12:41 +0000

    Servicio REST de Speakers

--- trunk/eventos/DevFestLima/DevFestLimaService/src/pe/gdglima/devfestlima/engine/bo/Speakers.java
+++ trunk/eventos/DevFestLima/DevFestLimaService/src/pe/gdglima/devfestlima/engine/bo/Speakers.java
@@ -6,76 +6,110 @@ import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
-@PersistenceCapable ( identityType =  IdentityType. APPLICATION )
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
 public class Speakers {
-	
+
 	@PrimaryKey
-    @Persistent ( valueStrategy =  IdGeneratorStrategy. IDENTITY )
-	private long id;
-	
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Long id;
+
+	@Persistent
 	private String bio;
+
+	@Persistent
 	private String firstName;
+
+	@Persistent
 	private String lastName;
+
+	@Persistent
 	private String displayName;
+
+	@Persistent
 	private String plusOneUrl;
+
+	@Persistent
 	private String thumbnailUrl;
+
+	@Persistent
 	private String userId;
+
+	@Persistent
 	private String speakerId;
-	
+
+	public Speakers() {
+	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getBio() {
 		return bio;
 	}
+
 	public void setBio(String bio) {
 		this.bio = bio;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getDisplayName() {
 		return displayName;
 	}
+
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
 	}
+
 	public String getPlusOneUrl() {
 		return plusOneUrl;
 	}
+
 	public void setPlusOneUrl(String plusOneUrl) {
 		this.plusOneUrl = plusOneUrl;
 	}
+
 	public String getThumbnailUrl() {
 		return thumbnailUrl;
 	}
+
 	public void setThumbnailUrl(String thumbnailUrl) {
 		this.thumbnailUrl = thumbnailUrl;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getSpeakerId() {
 		return speakerId;
 	}
+
 	public void setSpeakerId(String speakerId) {
 		this.speakerId = speakerId;
 	}
 
-	
 }
--- trunk/eventos/DevFestLima/DevFestLimaService/src/pe/gdglima/devfestlima/engine/bo/SpeakersEndpoint.java
+++ trunk/eventos/DevFestLima/DevFestLimaService/src/pe/gdglima/devfestlima/engine/bo/SpeakersEndpoint.java
@@ -1,122 +1,169 @@
 package pe.gdglima.devfestlima.engine.bo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
+import javax.annotation.Nullable;
 import javax.inject.Named;
 import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
+import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.datanucleus.query.JDOCursorHelper;
+import com.google.appengine.api.search.Consistency;
+import com.google.appengine.api.search.Document;
+import com.google.appengine.api.search.Field;
+import com.google.appengine.api.search.Index;
+import com.google.appengine.api.search.IndexSpec;
+import com.google.appengine.api.search.Results;
+import com.google.appengine.api.search.ScoredDocument;
+import com.google.appengine.api.search.SearchServiceFactory;
+import com.google.api.server.spi.response.CollectionResponse;
 
-@Api(name = "devsite_speakers")
+@Api(name = "speakers", description = "Esta entidad representa a los expositores.", version = "v1")
 public class SpeakersEndpoint {
 
-  /**
-   * This method lists all the entities inserted in datastore.
-   * It uses HTTP GET method.
-   *
-   * @return List of all entities persisted.
-   */
-  @SuppressWarnings({"cast", "unchecked"})
-  @ApiMethod ( name = "speakers.list" , path = "speakers/list" )
-  public List<Speakers> listSpeakers() {
-    PersistenceManager mgr = getPersistenceManager();
-    List<Speakers> result = new ArrayList<Speakers>();
-    
-    Speakers speakers= new Speakers();
-    speakers.setBio("Ingeniero de Software, desarrollo de aplicaciones Android, App Engine, etc.");
-    speakers.setFirstName("David");
-    speakers.setLastName("Motta B");
-    speakers.setDisplayName("@dmotta");
-    speakers.setPlusOneUrl("url");
-    speakers.setThumbnailUrl("foto");
-    speakers.setUserId("dmotta");
-    speakers.setUserId("+dmotta");
-    result.add(speakers);
-//    try{
-//      Query query = mgr.newQuery(Speakers.class);
-//      for (Object obj : (List<Object>) query.execute()) {
-//        result.add(((Speakers) obj));
-//      }
-//    } finally {
-//      mgr.close();
-//    }
-    return result;
-  }
+	@ApiMethod(httpMethod = "POST", name = "speakers.insert", path = "speakers/insert")
+	public Speakers insertSpeakers(Speakers speakers) {
+		PersistenceManager mgr = getPersistenceManager();
+		try {
+			mgr.makePersistent(speakers);
+			addToSearchIndex(speakers);
+			System.out.println(":::::::::::::::::::::::::ID: " + speakers.getId());
+		} finally {
+			mgr.close();
+		}
+		return speakers;
+	}
 
-  /**
-   * This method gets the entity having primary key id. It uses HTTP GET method.
-   *
-   * @param id the primary key of the java bean.
-   * @return The entity with primary key id.
-   */
-  public Speakers getSpeakers(@Named("id") Long id) {
-    PersistenceManager mgr = getPersistenceManager();
-    Speakers speakers  = null;
-    try {
-      speakers = mgr.getObjectById(Speakers.class, id);
-    } finally {
-      mgr.close();
-    }
-    return speakers;
-  }
+	@SuppressWarnings("unchecked")
+	@ApiMethod(httpMethod = "GET", name = "speakers.list", path = "speakers/search/{queryString}")
+	public List<Speakers> search(@Named("queryString") String queryString) throws Exception {
+		System.out.println("queryString:  " + queryString);
 
-  /**
-   * This inserts the entity into App Engine datastore.
-   * It uses HTTP POST method.
-   *
-   * @param speakers the entity to be inserted.
-   * @return The inserted entity.
-   */
-  public Speakers insertSpeakers(Speakers speakers) {
-    PersistenceManager mgr = getPersistenceManager();
-    try {
-      mgr.makePersistent(speakers);
-    } finally {
-      mgr.close();
-    }
-    return speakers;
-  }
+		PersistenceManager mgr = getPersistenceManager();
+		List<Speakers> returnList = new ArrayList<Speakers>();
 
-  /**
-   * This method is used for updating a entity.
-   * It uses HTTP PUT method.
-   *
-   * @param speakers the entity to be updated.
-   * @return The updated entity.
-   */
-  public Speakers updateSpeakers(Speakers speakers) {
-    PersistenceManager mgr = getPersistenceManager();
-    try {
-      mgr.makePersistent(speakers);
-    } finally {
-      mgr.close();
-    }
-    return speakers;
-  }
+		try {
 
-  /**
-   * This method removes the entity with primary key id.
-   * It uses HTTP DELETE method.
-   *
-   * @param id the primary key of the entity to be deleted.
-   * @return The deleted entity.
-   */
-  public Speakers removeSpeakers(@Named("id") Long id) {
-    PersistenceManager mgr = getPersistenceManager();
-     Speakers speakers  = null;
-    try {
-      speakers = mgr.getObjectById(Speakers.class, id);
-      mgr.deletePersistent(speakers);
-    } finally {
-      mgr.close();
-    }
-    return speakers;
-  }
+			Results<ScoredDocument> searchResults = INDEX.search(queryString);
 
-  private static PersistenceManager getPersistenceManager() {
-    return PMF.get().getPersistenceManager();
-  }
+			for (ScoredDocument scoredDoc : searchResults) {
+				Field fieldId = scoredDoc.getOnlyField("id");
+				if (fieldId == null || fieldId.getText() == null)
+					continue;
+
+				long speakersId = Long.parseLong(fieldId.getText());
+				if (speakersId != -1) {
+					Speakers a = getSpeakers(speakersId);
+					returnList.add(a);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			mgr.close();
+		}
+
+		return returnList;
+	}
+
+	@ApiMethod(httpMethod = "GET", name = "speakers.list", path = "speakers/list")
+	@SuppressWarnings({ "cast", "unchecked" })
+	public CollectionResponse<Speakers> listSpeakers(@Nullable @Named("cursor") String cursorString, @Nullable @Named("limit") Integer limit) throws Exception {
+
+		PersistenceManager pm = null;
+		Cursor cursor = null;
+		List<Speakers> execute = null;
+		try {
+			pm = getPersistenceManager();
+			Query query = pm.newQuery(Speakers.class);
+
+			if (cursorString != null && cursorString != "") {
+				cursor = Cursor.fromWebSafeString(cursorString);
+				Map<String, Object> extensionMap = new HashMap<String, Object>();
+				extensionMap.put(JDOCursorHelper.CURSOR_EXTENSION, cursor);
+				query.setExtensions(extensionMap);
+			}
+
+			if (limit != null) {
+				query.setRange(0, limit);
+			}
+
+			execute = (List<Speakers>) query.execute();
+			cursor = JDOCursorHelper.getCursor(execute);
+
+			if (cursor != null) {
+				cursorString = cursor.toWebSafeString();
+			} else {
+				cursorString = "";
+			}
+
+			execute.size();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pm.close();
+		}
+
+		return CollectionResponse.<Speakers> builder().setItems(execute).setNextPageToken(cursorString).build();
+	}
+
+	@ApiMethod(httpMethod = "GET", name = "speakers.get", path = "speakers/get/{id}")
+	public Speakers getSpeakers(@Named("id") Long id) {
+		PersistenceManager mgr = getPersistenceManager();
+		Speakers speakers = null;
+		try {
+			speakers = mgr.getObjectById(Speakers.class, id);
+		} finally {
+			mgr.close();
+		}
+		return speakers;
+	}
+
+	public Speakers updateSpeakers(Speakers speakers) {
+		PersistenceManager mgr = getPersistenceManager();
+		try {
+			mgr.makePersistent(speakers);
+		} finally {
+			mgr.close();
+		}
+		return speakers;
+	}
+
+	public Speakers removeSpeakers(@Named("id") Long id) {
+		PersistenceManager mgr = getPersistenceManager();
+		Speakers speakers = null;
+		try {
+			speakers = mgr.getObjectById(Speakers.class, id);
+			mgr.deletePersistent(speakers);
+		} finally {
+			mgr.close();
+		}
+		return speakers;
+	}
+
+	private static final Index INDEX = getIndex();
+
+	private static Index getIndex() {
+		IndexSpec indexSpec = IndexSpec.newBuilder().setName("speaker_index").setConsistency(Consistency.PER_DOCUMENT).build();
+		return SearchServiceFactory.getSearchService().getIndex(indexSpec);
+	}
+
+	public static void addToSearchIndex(Speakers speakers) {
+		Document.Builder docBuilder = Document.newBuilder().addField(Field.newBuilder().setName("id").setText(Long.toString(speakers.getId())))
+				.addField(Field.newBuilder().setName("name").setText(speakers.getUserId() != null ? speakers.getUserId() : ""));
+		docBuilder.setId(Long.toString(speakers.getId()));
+		Document doc = docBuilder.build();
+		INDEX.add(doc);
+	}
+
+	private static PersistenceManager getPersistenceManager() {
+		return PMF.get().getPersistenceManager();
+	}
 
 }
